d77aafe259cb3f30c56eb6f0d32b97167ec8fc79
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-04-23 19:12:13 -0400

    Added About page and updated routing in Home page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <div style={{ padding: '2rem', color: '#fff' }}>
+        <h1>About Page</h1>
+        <p>This is my finals for Adv.Front-End programming</p>
+      </div>
+    );
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import UserProfile from '../components/UserProfile';
 import Counter from '../components/Counter';
 import TodoList from '../components/TodoList';
+import Link from 'next/link';
+
 
 
 
 export default function Home() {
   return (
     <div style={{ padding: '2rem' }}>
+    <nav style={{ marginBottom: '2rem' }}>
+      <Link href="/" style={{ marginRight: '1rem', color: 'cyan' }}>Home</Link>
+      <Link href="/about" style={{ color: 'cyan' }}>About</Link>
+    </nav>
+
       <UserProfile
         name="Gabriel Hernandez"
         profilePicture="/Project.jpg"
